2e9f43a0f6ea9086f45f700f6b46530a58251148
saniyanaz <saniyanaz384@gmail.com>
2018-07-27 11:47:36 +0530

    pushing files

---
+++ calculator/js/script.js
@@ -0,0 +1,11 @@
+var addButton=document.getElementById("add");
+addButton.addEventListener("click",function(){
+
+var val1=document.getElementById("val1").value;
+var val2=document.getElementById("val2").value;
+var res=Number(val1)+Number(val2);
+var resfield=document.getElementById("res");
+resfield.value=res;
+alert("Button Clicked");
+
+});
